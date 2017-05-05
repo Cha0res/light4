f34e134418440b1ebcf63c1699c7232b6c5ee76a
Cha0res <qipengfei@outlook.com>
2017-05-05 20:29:50 +0800

    fixed cannot delete users

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,7 +14,7 @@ class ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     if @product.save
-      redirect_to @product
+      redirect_to products_path
     else
       render 'new'
     end
@@ -28,7 +28,7 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
 
     if @product.update(product_params)
-      redirect_to @product
+      redirect_to products_path
     else
       render 'edit'
     end
