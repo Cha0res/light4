4db5372092bcd9855dd7fad9e5dafc19aeff449a
Cha0res <qipengfei@outlook.com>
2017-05-06 15:40:53 +0800

    production env config

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,27 @@ module Light4
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.precompile << Proc.new do |path|
+      if path =~ /\.(css|js|scss|png|jpg|gif|json)\z/
+        full_path = Rails.application.assets.resolve(path).to_path
+        app_assets_path1 = Rails.root.join('app', 'assets').to_path
+        app_assets_path2 = Rails.root.join('public', 'assets').to_path
+        app_assets_path3 = Rails.root.join('vendor', 'assets').to_path
+
+        if full_path.starts_with? app_assets_path1
+          true
+        else
+          if full_path.starts_with? app_assets_path2
+            true
+          else
+            if full_path.starts_with? app_assets_path3
+              true
+            else
+              false
+            end
+          end
+        end
+      end
+    end
   end
 end
